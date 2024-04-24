9b7cbd5d15316ba04ef2c059f4b7a5efe0f7b63a
turrnut <victorconqueror021@gmail.com>
2024-04-24 17:46:05 -0400

    Placeholder complete

--- articlescript.js
+++ articlescript.js
@@ -1,4 +1,4 @@
-document.getElementById("theheader").innerHTML = "<nav id=\"navbar\"><div id=\"menu-toggle\" class=\"menu-icon\">&#9776;</div><div class=\"logo\">The Turrnut Times</div><ul id=\"menu-items\" class=\"hidden\"><li><a href=\"../index.html\">Home</a></li><li><a href=\"#\">World</a></li><li><a href=\"#\">Local</a></li><li><a href=\"#\">Sports</a></li></ul></nav>";
+document.getElementById("theheader").innerHTML = "<nav id=\"navbar\"><div id=\"menu-toggle\" class=\"menu-icon\">&#9776;</div><div class=\"logo\">Unnamed Website</div><ul id=\"menu-items\" class=\"hidden\"><li><a href=\"../index.html\">Home</a></li><li><a href=\"#\">World</a></li><li><a href=\"#\">Local</a></li><li><a href=\"#\">Sports</a></li></ul></nav>";
 
 document.getElementById('menu-toggle').addEventListener('click', function() {
     var menu = document.getElementById('menu-items');
@@ -9,4 +9,4 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     }
 });
 
-document.getElementById('thefooter').innerHTML = "<p>&copy; 2024 The Turrnut Times. All rights reserved.</p>";
+document.getElementById('thefooter').innerHTML = "<p>&copy; 2024 Unnamed Website. All rights reserved.</p>";
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("theheader").innerHTML = "<nav id=\"navbar\"><div id=\"menu-toggle\" class=\"menu-icon\">&#9776;</div><div class=\"logo\">The Turrnut Times</div><ul id=\"menu-items\" class=\"hidden\"><li><a href=\"index.html\">Home</a></li><li><a href=\"#\">World</a></li><li><a href=\"#\">Local</a></li><li><a href=\"#\">Sports</a></li></ul></nav>";
+document.getElementById("theheader").innerHTML = "<nav id=\"navbar\"><div id=\"menu-toggle\" class=\"menu-icon\">&#9776;</div><div class=\"logo\">Unnamed Website</div><ul id=\"menu-items\" class=\"hidden\"><li><a href=\"index.html\">Home</a></li><li><a href=\"#\">World</a></li><li><a href=\"#\">Local</a></li><li><a href=\"#\">Sports</a></li></ul></nav>";
 
 // Toggle mobile menu visibility
 document.getElementById('menu-toggle').addEventListener('click', function() {
@@ -14,7 +14,7 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 
 // footer
 
-document.getElementById('thefooter').innerHTML = "<p>&copy; 2024 The Turrnut Times. All rights reserved.</p>";
+document.getElementById('thefooter').innerHTML = "<p>&copy; 2024 Unnamed Website. All rights reserved.</p>";
 
 /////////////
 
